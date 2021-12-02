5837d85bd8ebe9d6597e86de96556bd2ca590fca
David Villamarin <dvillamarin@stackbuilders.com>
2021-12-01 22:03:43 -0500

    handling side effects inside components

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,4 +1,4 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import { cartActions } from "../../store/cart-slice";
 import Card from "../UI/Card";
@@ -6,16 +6,11 @@ import classes from "./ProductItem.module.css";
 
 const ProductItem = (props) => {
   const dispatch = useDispatch();
+
   const { title, price, description, id } = props;
 
   const addToCartHandler = () => {
-    dispatch(
-      cartActions.addItemToCart({
-        id,
-        title,
-        price,
-      })
-    );
+    dispatch(cartActions.addItemToCart({ id, title, price }));
   };
 
   return (
